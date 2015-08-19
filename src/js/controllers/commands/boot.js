2fb4c835d08a59252c6a0aedaa0ecd0a8dac3878
Artem Abashev <artem@abashev.com>
2015-08-19 22:52:46 +0300

    Updated boot sequence.

--- src/js/controllers/commands/boot.js
+++ src/js/controllers/commands/boot.js
@@ -5,24 +5,20 @@ angular.module('SNAP.controllers')
 
   return function() {
     return new Promise((resolve, reject) => {
-      function loadLocation() {
-        LocationManager.loadConfig()
-          .then(() => {
-            LocationManager.loadSeats().then(() => resolve(), reject);
-          }, reject);
-      }
-
       AuthenticationManager.validate().then(authorized => {
         if (authorized === false) {
-          AuthenticationManager.authorize()
-            .then(() => {
-              resolve('reboot');
-            }, reject);
+          AuthenticationManager.authorize().then(() => {
+            resolve('reboot');
+          }, reject);
           return;
         }
 
-        return loadLocation();
-      });
-    }, reject);
+        LocationManager.loadConfig().then(() => {
+          LocationManager.loadSeats().then(() => {
+            resolve();
+          }, reject);
+        }, reject);
+      }, reject);
+    });
   };
 }]);
