cd147da48a0cf6ab471d6f7ce15828027bce3d7c
Artem Abashev <artem@abashev.com>
2015-08-27 22:31:32 +0300

    Improved menu navigation.

--- src/js/components/menutitle.js
+++ src/js/components/menutitle.js
@@ -4,16 +4,25 @@ angular.module('SNAP.components')
     render: function() {
       var title = this.props.title || '',
           history = this.props.history || [];
+
+      history = history.splice(0).reverse();
+
       return React.DOM.div({}, [
         React.DOM.button({
           key: 1,
           className: 'clickable',
           onClick: e => {
             e.preventDefault();
-            NavigationManager.goBack();
+
+            if (NavigationManager.location.type === 'menu') {
+              NavigationManager.location = { type: 'home' };
+            }
+            else {
+              NavigationManager.goBack();
+            }
           }
         }),
-        React.DOM.ul({ key: 2 }, history.splice(0).reverse().map((destination, i) => {
+        React.DOM.ul({ key: 2 }, history.map((destination, i) => {
           return React.DOM.li({
             key: i,
             onClick: e => {
